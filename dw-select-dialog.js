7d8ba42eb67a238b7ca6512019560cbac87fd7f4
Chandresh Kikani <chandresh.kikani@dreamworldsolutions.com>
2019-09-06 12:04:20 +0530

    fix: Added dummy space

--- dw-select-dialog.js
+++ dw-select-dialog.js
@@ -716,7 +716,7 @@ export class DwSelectDialog extends DwSelectBaseDialog {
       this._triggerOpenedChange();
     }
 
-    //Scroll Keyboard highlighted item into view when dialog is opened and Keyboard highlighted index is changed and current mode is not mobile.
+    //Scroll Keyboard highlighted item into view when dialog is opened and Keyboard highlighted index is changed and current mode is not mobile .
     if(!this.mobileMode && this.opened && (changedProps.has('_kbHighlightedIndex') || changedProps.has('opened'))) {
       let itemEl = this.shadowRoot.querySelectorAll('.item')[this._kbHighlightedIndex];
       if(itemEl) {
