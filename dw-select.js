132ef478556fdcc610b53b6840ff312146165eda
Shakil Mansuri <shakil.mansuri@dreamworldsolutions.com>
2021-03-19 15:39:33 +0530

    fix: use original box-shadow value of dialog.

--- dw-select.js
+++ dw-select.js
@@ -167,7 +167,7 @@ export class DwSelect extends DwFormElement(LitElement) {
     }
 
     .tippy-box[data-animation="fadeIn"] {
-      box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12);
+      box-shadow: 0px 2px 6px #ccc;
       will-change: opacity, transform, transform-origin;
       animation-name: fadeIn;
       animation-duration: var(--dw-popover-animation-time, 0.3s);
