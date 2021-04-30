a95b0f85d715412d3d0365ee283aa34e4ece7a33
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-04-30 17:39:43 +0530

    fix(dw-select): fix horizontal scroll issue in firefox. (#56)
     fix(dw-select): fix horizontal scroll issue in firefox.

--- dw-select.js
+++ dw-select.js
@@ -166,6 +166,10 @@ export class DwSelect extends DwFormElement(LitElement) {
       border-radius: 4px;
     }
 
+    .tippy-box {
+      overflow-x: hidden !important;
+    }
+
     .tippy-box:focus {
       outline: none;
     }
@@ -204,23 +208,6 @@ export class DwSelect extends DwFormElement(LitElement) {
     }
   `];
   }
-
-  /**
-   * Fired when the value changes.
-   *
-   * @event value-changed
-   * @type {Object}
-   * @property {String} value - The String value for the selected item of the multiselect.
-   * @property {String} selected - The selected item from the items array.
-   */
-
-  /**
-   * Fired when the opened property changes.
-   *
-   * @event opened-changed
-   * @type {Object}
-   * @property {Boolean} opened - True if the dropdown is open, false otherwise.
-   */
 
   /**
    * Fired when the invalid property changes.
@@ -756,7 +743,7 @@ export class DwSelect extends DwFormElement(LitElement) {
           } else {
             maxHeight = window.innerHeight - tippyBox.getBoundingClientRect().top;
           }
-          tippyBox.style.overflow = 'auto';
+          tippyBox.style.overflowY = 'auto';
           tippyBox.style.maxHeight = `${maxHeight}px`;
         }
       },
