4865041678074ebfe2bdd8e07316b5bbda1d8e5f
shakil-mansuri-dream <53848663+shakil-mansuri-dream@users.noreply.github.com>
2021-12-29 10:36:56 +0530

    feat: add appendTo property support.
    card link: https://a.kerika.net/C7r/board/N2O6/Y5I6

--- dw-select.js
+++ dw-select.js
@@ -1,10 +1,11 @@
-import { html, css } from 'lit-element';
+import { html } from 'lit-element';
 import { LitElement } from '@dreamworld/pwa-helpers/lit-element.js';
 import { Typography } from '@dreamworld/material-styles/typography';
 import './dw-select-dialog';
 import '@dreamworld/dw-icon';
 import '@dreamworld/dw-icon-button';
 import { DwFormElement } from '@dreamworld/dw-form/dw-form-element'; 
+import { dwSelectStyle, externalStyle } from './dw-select-css.js';
 import '@dreamworld/dw-button';
 import tippy from 'tippy.js';
 
@@ -14,193 +15,10 @@ import tippy from 'tippy.js';
 export class DwSelect extends DwFormElement(LitElement) {
 
   static get styles() {
-    return [Typography, css`
-    :host {
-      display: inline-block;
-      box-sizing: border-box;
-      width: var(--dw-select-width, 250px);
-      --dw-select-error-color: var(--error-color);
-      --dw-select-error-color: var(--error-color);
-      --dw-select-label-color: var(--secondary-text-color);
-      --dw-select-expand-more-icon-color: var(--light-theme-secondary-color);
-      --dw-select-border-color: var(--light-theme-divider-color);
-      --dw-select-value-color: var(--primary-text-color);
-      --dw-select-placeholder-color: var(--light-theme-disabled-color);
-      --dw-error-message-color: var(--error-color);
-    }
-
-    :host([trigger-icon]), :host([trigger-label]), :host([custom-trigger]) {
-      width: var(--dw-select-width, 100%);
-    }
-
-    .main-container {
-      display: inline-block;
-      width: 100%;
-    }
-
-    :host([trigger-icon]) .main-container #dropdownContainer, 
-    :host([trigger-label]) .main-container #dropdownContainer, 
-    .main-container #dropdownContainer .trigger-icon, 
-    .main-container #dropdownContainer .trigger-label {
-      display: inline-block;
-      box-sizing: border-box;
-      width: 100%;
-    }
-
-    .main-container #dropdownContainer .trigger-icon, .main-container #dropdownContainer .trigger-label {
-      -ms-flex-pack: center;
-      -webkit-justify-content: center;
-      justify-content: center;
-    }
-
-    .main-container #dropdownContainer dw-icon-button.trigger-icon {
-      --dw-icon-color: var(--dw-select-trigger-icon-color, #000);
-    }
-
-    .main-container #dropdownContainer .trigger-label {
-      width: var(--dw-select-trigger-label-width, auto);
-      height: var(--dw-select-trigger-label-height, 36px);
-    }
-
-    .main-container #dropdownContainer .trigger-icon-label dw-icon {
-      margin-right: 8px;
-      --dw-icon-color: var(--dw-select-trigger-icon-color);
-    }
-
-    :host([invalid]) .main-container #dropdownContainer .label {
-      color: var(--dw-select-error-color);
-    }
-
-    :host([invalid]) .main-container #dropdownContainer .dropdown-input {
-      border-bottom: 2px solid var(--dw-select-error-color);
-    }
-
-    .main-container #dropdownContainer {
-      outline: none;
-      padding: var(--dw-select-dropdown-container-padding, 0px 0px 4px 0px);
-      margin: var(--dw-select-dropdown-container-margin, 0px);
-      cursor: pointer;
-    }
-
-    .main-container #dropdownContainer .label {
-      color: var(--dw-select-label-color);
-      padding-top: 4px;
-    }
-
-    .main-container  #dropdownContainer .dropdown-input dw-icon { 
-      --dw-icon-color: var(--dw-select-expand-more-icon-color);
-      padding: 0px 4px;
-    }
-  
-    .main-container #dropdownContainer .dropdown-input {
-      display: flex;
-      display: -ms-flexbox;
-      display: -webkit-flex;
-      flex-direction: row;
-      -ms-flex-direction: row;
-      -webkit-flex-direction: row;
-      align-items: center;
-      -ms-flex-align: center;
-      -webkit-align-items: center;
-      justify-content: space-between;
-      -ms-flex-pack: space-between;
-      -webkit-justify-content: space-between;
-      border-bottom: 1px solid var(--dw-select-border-color);
-    }
-
-    .main-container #dropdownContainer .dropdown-input .value-container {
-      padding: 8px 0px 6px 0px;
-      overflow: hidden;
-    }
-
-    .main-container #dropdownContainer .dropdown-input .value-container .value {
-      color: var(--dw-select-value-color);
-      padding-top: 4px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-
-    .main-container #dropdownContainer .dropdown-input .value-container .placeholder {
-      color: var(--dw-select-placeholder-color);
-    }
-
-    .error-message {
-      color: var(--dw-error-message-color);
-      padding-bottom: 4px;
-    }
-
-    #overlay {
-      position: fixed;
-      display: none;
-      top: 0; right: 0; bottom: 0; left: 0;
-      background-color: var(--overlay-color, rgba(0,0,0,0.4));
-      overflow: hidden;
-      width: 100%; 
-      height: 100%;
-      z-index: 99;
-      cursor: pointer;
-    }
-
-    :host([overlay][mobile-mode]) #overlay {
-      display:block;
-    }
-
-    :host([readOnly]) .main-container #dropdownContainer {
-      cursor: default;
-    }
-
-    :host([readOnly]) .main-container #dropdownContainer .label,
-    :host([readOnly]) .main-container #dropdownContainer .dropdown-input {
-      opacity: 0.6;
-    }
-
-    #select-dialog,
-    .tippy-box {
-      border-radius: 4px;
-    }
-
-    .tippy-box {
-      overflow-x: hidden !important;
-    }
-
-    .tippy-box:focus {
-      outline: none;
-    }
-
-    .tippy-box[data-animation="expand"] {
-      box-shadow: 0px 2px 6px #ccc;
-      will-change: transform, transform-origin;
-      animation-name: expand;
-      animation-duration: var(--dw-popover-animation-time, 0.3s);
-    }
-
-    .tippy-box[data-placement="bottom-start"] {
-      transform-origin: top left;
-    }
-
-    .tippy-box[data-placement="bottom-end"] {
-      transform-origin: top right;
-    }
-
-    .tippy-box[data-placement="top-start"] {
-      transform-origin: bottom left;
-    }
-
-    .tippy-box[data-placement="top-end"] {
-      transform-origin: bottom right;
-    }
-
-    @keyframes expand {
-      from {
-        transform: scale(0, 0)
-      }
-
-      to {
-        transform: scaleY(1, 1)
-      }
-    }
-  `];
+    return [
+      Typography, 
+      dwSelectStyle
+    ];
   }
 
   /**
@@ -424,6 +242,20 @@ export class DwSelect extends DwFormElement(LitElement) {
       triggerLabel: {type: String, reflect: true, attribute: 'trigger-label'},
 
       /**
+       * Input property.
+       * This work only if `mobileMode` property is false.
+       * Dropdown element in which content will be appened. Default is parent element of trigger element.
+       */
+      appendTo: { type: Object },
+
+      /**
+       * Input property.
+       * This work only if `mobileMode` property is false.
+       * Dropdown element z-index, default value is 9999.
+       */
+      zIndex: { type: Number },
+
+      /**
        * The element that should be used to position the element
        */
       _positionTarget: { type: Object },
@@ -537,6 +369,8 @@ export class DwSelect extends DwFormElement(LitElement) {
     this.clearIconSize = 18;
     this.readOnly = false;
     this.animation = 'expand';
+    this.appendTo = 'parent';
+    this.zIndex = 9999;
   }
 
   /**
@@ -713,9 +547,10 @@ export class DwSelect extends DwFormElement(LitElement) {
       content: self._dialog,
       maxWidth: 'none',
       trigger: 'manual',
+      zIndex: this.zIndex,
       interactive: true,
       hideOnClick: false, //Note: interactive does not work in shadowDOM, so explicitly sets it to `false` & closes dialog from `onClickOutside` handler.
-      appendTo: 'parent',
+      appendTo: this.appendTo,
       popperOptions: {
         modifiers: [{ name: 'flip', enabled: false }]
       },
@@ -741,6 +576,13 @@ export class DwSelect extends DwFormElement(LitElement) {
           tippyBox.style.maxHeight = `${maxHeight}px`;
         }
       },
+      onCreate() {
+        self._sheet = document.createElement('style');
+        self._sheet.id = 'dw-select-style';
+        self._sheet.innerHTML = externalStyle.cssText;
+        const parentEl = self.appendTo === 'parent' ? triggerEl.parentNode : self.appendTo;
+        parentEl.appendChild(self._sheet);
+      },
       onClickOutside(instance, event) {
         const path = event.composedPath && event.composedPath() || event.path;
         for (let el of path) {
@@ -756,6 +598,10 @@ export class DwSelect extends DwFormElement(LitElement) {
           setTimeout(() => {
             self.opened = false;
           })
+        }
+
+        if (self.isConnected) {
+          self._sheet && self._sheet.remove && self._sheet.remove();
         }
       },
       animation: this.animation,
