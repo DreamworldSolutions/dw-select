60ebe94822519414f153e046ab097fa52b6178c7
Hiten Bhayani <hiten@hisab.co>
2022-07-07 14:05:28 +0530

    feat: START dw-select-dialog: mock implementation

--- dw-select-dialog.js
+++ dw-select-dialog.js
@@ -6,11 +6,122 @@ import { DwCompositeDialog } from "@dreamworld/dw-dialog/dw-composite-dialog.js"
 /**
  * Renders the list of choices on temporary Composite Dialog.
  * Using `dw-list-item` or custom template provider `renderItem` to render List of Items
- * 
+ *
  * [`select-dialog-doc`](docs/select-dialog.md)
  */
 
 export class DwSelectDialog extends DwCompositeDialog {
+  static properties = {
+    /**
+     * Selected list item object.
+     * `object` in case of single selection;
+     * `object[]` in case multiple selection.
+     */
+    value: { type: Object },
+
+    /**
+     * Input Element
+     * When it’s specified it binds keyboard event on this; Otherwise, it renders it’s own input in the header.
+     * Integrator will set this value only for non-touch devices.
+     */
+    elInput: { type: Object },
+
+    /**
+     * Whether or not to show the `searchable` variant.
+     */
+    searchable: { type: Boolean },
+
+    /**
+     * Represents current layout in String. Possible values: `small`, `medium`, `large`, `hd`, and `fullhd`.
+     */
+    layout: { type: String },
+
+    /**
+     * `vkb` stands for Virtual KeyBoard. Whether the Device has Virtual keyboard or not.
+     */
+    vkb: { type: Boolean },
+
+    /**
+     * Input Property
+     * A Group has properties: name, label, collapsible, collapsed
+     */
+    groups: { type: Object },
+
+    /**
+     * Replica of `groups`
+     * Used to render items in group. When groups is changed it’s updated (through cloned array).
+     * When user interacts, this property is changed but `groups` isn’t.
+     */
+    _groups: { type: Object },
+
+    /**
+     * To select group name
+     * returns group name in string
+     */
+    groupSelector: { type: Function },
+
+    /**
+     * Expression of Group
+     */
+    groupExpression: { type: String },
+
+    /**
+     * Original List of selectable items.
+     */
+    items: { type: Object },
+
+    /**
+     * Represents items to be rendered by lit-virtualizer.
+     * { type: GROUP or ITEM, value: Group or Item object }
+     * It’s computed from _groups, items & query.
+     */
+    _items: { type: Object },
+
+    /**
+     * Activated item
+     * One of the Item from _items, by reference.
+     */
+    _activatedItem: { type: Object },
+
+    /**
+     * Provides value that actually represent in list items
+     */
+    valueProvider: { type: Function },
+
+    /**
+     * Expression of the value
+     * default: _id
+     */
+    valueExpression: { type: String },
+
+    /**
+     * Messages of for noRecords and noMatchingRecords
+     * Example: {noRecords: "", noMatchingRecords: ""}
+     */
+    messages: { type: Object },
+
+    /**
+     * Provides any Block element to represents list items
+     * Should show it's hover effect; and ripple on click
+     * Highlight text based on `query`
+     * Integrator listens on the ‘click’ event to know whether selection is changed or not.
+     * It must not be focusable.
+     */
+    renderItem: { type: Function },
+
+    /**
+     * Provides any Block elements to represents group items
+     * name property should be set to input name.
+     * Should show hover & ripple effects only if it’s collapsible.
+     * Integrator listens on ‘click’ event to toggle collapsed status.
+     */
+    renderGroupItem: { type: Function },
+
+    /**
+     * search query in string. used to filter items and highlight query keywords
+     */
+    _query: { type: String },
+  };
 
   constructor() {
     super();
@@ -18,8 +129,8 @@ export class DwSelectDialog extends DwCompositeDialog {
   }
 
   get _contentTemplate() {
-    return html`Dw-Select-dialog`
+    return html`Dw-Select-dialog`;
   }
 }
 
-window.customElements.define('dw-select-dialog', DwSelectDialog)
+window.customElements.define("dw-select-dialog", DwSelectDialog);
