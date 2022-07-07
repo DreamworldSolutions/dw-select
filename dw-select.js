bc562896e08adab1c63af7a86ca80ba0b27d3bc1
Hiten Bhayani <hiten@hisab.co>
2022-07-07 13:45:28 +0530

    feat: START dw-select-trigger: documentation

--- dw-select.js
+++ dw-select.js
@@ -4,13 +4,57 @@ import { LitElement, html } from "lit";
 import "./dw-select-trigger.js";
 import "./dw-select-dialog.js";
 
+/**
+ * A Select is an input widget with an associated dropdown that allows users to select a value from a list of possible values.
+ * 
+ * ## Types of Select
+ * 
+ * ### Default
+ *  - Sometimes Select does not support to allowed user intrect with text input (Non-searchable). 
+ *  - It is used when the list of options is predefined and limited.
+ * 
+ * ### Searchable
+ *  - Sometimes Select support to allowed user intrect with text input (searchable).
+ *  - Typing input serves to filter suggestions presented in the popup.
+ *  - It is used when the list of options is variable and very long.
+ * 
+ * ## Behaviour
+ *  - Renders `dw-select-trigger` and `dw-select-dialog`
+ *  - When `opened` is true attach and open dialog
+ * 
+ * ### Focus:
+ *  - For searchable types, it shows a cursor. For non searchable type, doesn’t show cursor.
+ *  - The dropdown opens when the user clicks the field using a pointing device.
+ * 
+ * ### Typing:
+ *  - Not applicable to non searchable
+ *  - Typing a character when the field is focused also opens the popup.
+ *  - Shows result matching with any word of the input string. For e.g. if the user types ‘hdfc bank’ it should show a records which contains words ‘hdfc’ or ‘bank’ anywhere (at start, middle or at end)  
+ *  - Matching string is highlighted
+ *  - Shows the most relevant matches in dropdown in sorted order. Sorting order is most matched to least matched.
+ * 
+ * ### Keyboard accessibility
+ *  TODO - write documentation
+ * 
+ * ### Toggle icon
+ *  TODO - write documentation
+ * 
+ * ### Reset Icon
+ *  TODO - write documentation
+ * 
+ * 
+ */
+
 export class DwSelect extends LitElement {
   render() {
     return html`
-      Dw-select
       <dw-select-trigger @click=${this._onTrigger}></dw-select-trigger>
-      <dw-select-dialog id="selectDialog"></dw-select-dialog>
+      ${this._loadFragments}
     `;
+  }
+
+  get _loadFragments() {
+    return html`<dw-select-dialog id="selectDialog"></dw-select-dialog>`
   }
 
   _onTrigger(e) {
