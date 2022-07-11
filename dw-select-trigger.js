ba1f8980f7b66202a716890cfc892c86f2aa64fc
Hiten Bhayani <hiten@hisab.co>
2022-07-11 14:11:19 +0530

    feat: START renders items [ci skip]
    - custom width integration

--- dw-select-trigger.js
+++ dw-select-trigger.js
@@ -2,76 +2,73 @@ import { LitElement, html, css } from "lit";
 
 /**
  * Used to edit and enter text, or only readOnnly.
- * 
+ *
  * [`select-dialog-doc`](docs/select-trigger.md)
  */
 
 export class DwSelectTrigger extends LitElement {
   static properties = {
-
     /**
-     * The input control's value. 
-     * Text to be shown as the value. 
+     * The input control's value.
+     * Text to be shown as the value.
      * It’s output property also when readOnly=false.
      */
-    value: {type: String},
+    value: { type: String },
 
     /**
      * Sets floating label value.
      */
-    label: {type: String},
+    label: { type: String },
 
     /**
      * Sets disappearing input placeholder.
      */
-    placeholder: {type: String},
+    placeholder: { type: String },
 
     /**
-     * Helper text to display below the input. 
+     * Helper text to display below the input.
      * Display default only when focused.
      */
-    helper: {type: String},
+    helper: { type: String },
 
     /**
      * Whether or not to show the material outlined variant.
      */
-    outlined: {type: Boolean},
+    outlined: { type: Boolean },
 
     /**
      * Input Property. When true, shows updated highlights
      */
-    updatedHighlight: {type: Boolean},
+    updatedHighlight: { type: Boolean },
 
     /**
      * Whether or not to show the temprory select dialog.
      */
-    opened: {type: Boolean},
+    opened: { type: Boolean },
 
     /**
      * Whether or not to show the read-only variant.
      */
-    readOnly: {type: Boolean},
+    readOnly: { type: Boolean },
 
     /**
      * When true user isn’t allowed to type anything.
      */
-    inputAllowed: {type: Boolean},
+    inputAllowed: { type: Boolean },
 
     /**
      * When true, helper text isn’t visible. Instead `errorMesage` is shown.
      */
-    error: {type: Boolean},
+    error: { type: Boolean },
 
     /**
      * Message to show in the error color at helper text when the textfield is invalid.
      */
-    errorMessage: {type: String}
-  }
+    errorMessage: { type: String },
+  };
 
   render() {
-    return html`
-      <input type="text" @input=${this._onInput}>
-    `;
+    return html` <input type="text" @input=${this._onInput} value=${this.value} /> `;
   }
 
   _onInput(e) {
@@ -79,4 +76,4 @@ export class DwSelectTrigger extends LitElement {
   }
 }
 
-customElements.define('dw-select-trigger', DwSelectTrigger)
+customElements.define("dw-select-trigger", DwSelectTrigger);
