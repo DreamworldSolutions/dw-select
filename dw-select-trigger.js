2e0122b1f0787dc96e88decee03f0c7c7f189f82
Hiten Bhayani <99726733+hitendreamworld@users.noreply.github.com>
2022-11-02 18:44:55 +0530

    feat: trigger element trailing icon color (#123)
    - introduce --dw-icon-color for trailing icon

--- dw-select-trigger.js
+++ dw-select-trigger.js
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from "lit";
 
 // view Elements
-import "@material/mwc-textfield";
+import { TextField } from "@material/mwc-textfield";
 import "@dreamworld/dw-ripple";
 
 /**
@@ -10,19 +10,24 @@ import "@dreamworld/dw-ripple";
  * [`select-dialog-doc`](docs/select-trigger.md)
  */
 
-export class DwSelectTrigger extends LitElement {
+export class DwSelectTrigger extends TextField {
   static get styles() {
     return [
+      TextField.styles,
       css`
-        mwc-textfield {
+        :host {
           display: block;
         }
 
-        :host([updatedHighlight]:not([outlined])) mwc-textfield {
+        :host([updatedHighlight]:not([outlined])) {
           --mdc-text-field-fill-color: var(
             --dw-select-updated-highlight-bg-color,
             rgba(2, 175, 205, 0.04)
           );
+        }
+
+        .mdc-text-field:not(.mdc-text-field--disabled) .mdc-text-field__icon--trailing {
+          color: var(--dw-icon-color, rgba(0, 0, 0, 0.54));
         }
       `,
     ];
@@ -31,62 +36,22 @@ export class DwSelectTrigger extends LitElement {
   static get properties() {
     return {
       /**
-       * The input control's value.
-       * Text to be shown as the value.
-       * It’s output property also when readOnly=false.
-       */
-      value: { type: String },
-
-      /**
-       * Sets floating label value.
-       */
-      label: { type: String },
-
-      /**
-       * Sets disappearing input placeholder.
-       */
-      placeholder: { type: String },
-
-      /**
-       * Helper text to display below the input.
-       * Display default only when focused.
-       */
-      helper: { type: String },
-
-      /**
-       * Whether or not to show the material outlined variant.
-       */
-      outlined: { type: Boolean },
-
-      /**
        * Input Property. When true, shows updated highlights
+       * Default "false"
        */
       updatedHighlight: { type: Boolean, reflect: true },
 
       /**
        * Whether or not to show the temprory select dialog.
+       * Default false
        */
       opened: { type: Boolean },
 
       /**
-       * Whether or not to show the read-only variant.
-       */
-      readOnly: { type: Boolean },
-
-      /**
        * When true user isn’t allowed to type anything.
+       * Default "false"
        */
       inputAllowed: { type: Boolean },
-
-      /**
-       * Displays error state if value is empty and input is blurred.
-       */
-      required: { type: Boolean },
-
-      /**
-       * Whether or not to show the `disabled` variant.
-       */
-      disabled: { type: Boolean },
 
       /**
        * When true, helper text isn’t visible. Instead `errorMesage` is shown.
@@ -100,26 +65,28 @@ export class DwSelectTrigger extends LitElement {
     };
   }
 
-  render() {
-    return html`
-      <mwc-textfield
-        label=${this.label}
-        placeholder=${this.placeholder}
-        helper=${this.helper}
-        ?readOnly=${!this.inputAllowed}
-        @input=${this._onInput}
-        value=${this.value}
-        ?outlined=${this.outlined}
-        ?required=${this.required}
-        ?disabled=${this.disabled}
-        .validationMessage=${this.errorMessage}
-        iconTrailing=${this.opened ? "expand_less" : "expand_more"}
-      ></mwc-textfield>
-    `;
+  constructor() {
+    super();
+    this.opened = false;
+    this.updatedHighlight = false;
+    this.inputAllowed = false;
+    this.iconTrailing = "expand_less";
   }
 
-  _onInput(e) {
-    this.value = e.target.value;
+  willUpdate(_changedProperties) {
+    super.willUpdate(_changedProperties);
+
+    if (_changedProperties.has("opened")) {
+      this.iconTrailing = this.opened ? "expand_less" : "expand_more";
+    }
+
+    if (_changedProperties.has("inputAllowed")) {
+      this.readOnly = !this.inputAllowed;
+    }
+
+    if (_changedProperties.has("errorMessage")) {
+      this.validationMessage = this.errorMessage;
+    }
   }
 }
 
