bf47583c90d9156e007d5722a29d116465bf36a3
Hiten Bhayani <99726733+hitendreamworld@users.noreply.github.com>
2022-11-28 09:55:41 +0530

    feat: use lit-virtualizer instead of repeat directive (#125)

--- dw-select-dialog.js
+++ dw-select-dialog.js
@@ -1,5 +1,6 @@
-import { css, html, unsafeCSS } from "@dreamworld/pwa-helpers/lit.js";
+import { css, html, nothing, unsafeCSS } from "@dreamworld/pwa-helpers/lit.js";
 import { repeat } from "lit/directives/repeat.js";
+import "@lit-labs/virtualizer";
 
 // View Elements
 import { DwCompositeDialog } from "@dreamworld/dw-dialog/dw-composite-dialog.js";
@@ -130,6 +131,11 @@ export class DwSelectDialog extends DwCompositeDialog {
 
         :host([type="fit"]) .mdc-dialog__title {
           padding: 8px 16px;
+        }
+
+        dw-list-item,
+        dw-select-group-item {
+          width: 100%;
         }
 
         dw-list-item[selected] {
@@ -522,10 +528,15 @@ export class DwSelectDialog extends DwCompositeDialog {
   }
 
   get _renderList() {
+    if (this.type === "popover" && !this._tippyShown) {
+      return;
+    }
     return html`
-      ${repeat(this._items, (item, index) =>
-        this.renderItem ? this.renderItem(item) : this._defaultTemplate(item, index)
-      )}
+      <lit-virtualizer
+        .items=${this._items}
+        .renderItem=${(item, index) =>
+          this.renderItem ? this.renderItem(item) : this._defaultTemplate(item, index)}
+      ></lit-virtualizer>
     `;
   }
 
