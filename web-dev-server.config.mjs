d573795f0b2d6d35520eb3ce8f30ea0ef4b744bb
Hiten Bhayani <hiten@hisab.co>
2022-12-23 18:16:18 +0530

    feat: trigger

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,13 +1,13 @@
-import rollupCommonjs from '@rollup/plugin-commonjs';
-import { fromRollup } from '@web/dev-server-rollup';
+import rollupCommonjs from "@rollup/plugin-commonjs";
+import { fromRollup } from "@web/dev-server-rollup";
 
 const commonjs = fromRollup(rollupCommonjs);
 
 export default {
-  appIndex: 'demo/index.html',
+  appIndex: "demo/index.html",
   open: true,
   nodeResolve: true,
   port: 8005,
   // esbuildTarget: "auto",
-  plugins: [commonjs()]
+  plugins: [commonjs()],
 };
