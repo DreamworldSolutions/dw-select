63fb7ef0d79eef3b69458a50895036c6cd74cff8
Hiten Bhayani <hiten@hisab.co>
2023-02-20 12:35:51 +0530

    feat: renderItem refactor
    - renderItem has 4 parameters (item, selected, activated, query)

--- demo/dw-select-extension-demo.js
+++ demo/dw-select-extension-demo.js
@@ -1,7 +1,9 @@
-import { LitElement, html, css } from "@dreamworld/pwa-helpers/lit.js";
+import "@dreamworld/dw-list-item";
+import { html } from "@dreamworld/pwa-helpers/lit.js";
+import isEqual from "lodash-es/isEqual.js";
 import { DwSelect } from "../dw-select";
 
-import { country_list, country_list_with_code, list, groupList, groups, accounts } from "./utils";
+import { country_list_with_code } from "./utils";
 
 export class DwSelectExtensionDemo extends DwSelect {
   constructor() {
@@ -11,6 +13,31 @@ export class DwSelectExtensionDemo extends DwSelect {
     this.label = "Extenstion Select";
     this.layout = "small";
     this.searchable = true;
+    this.renderItem = (item, selected, activated, query) =>
+      this._renderProductListItem(item, selected, activated, query);
+  }
+
+  _renderProductListItem(item, selected, activated, query) {
+    return html`
+      <dw-list-item
+        .title1=${item.value.name}
+        title2="${item.value?.code || ""}"
+        twoLine
+        ?selected=${selected}
+        .trailingIcon=${"done"}
+        .leadingIconFont=${"OUTLINED"}
+        ?hasTrailingIcon=${selected}
+        .highlight=${query}
+        ?activated=${activated}
+        @click=${(e) => this._onClick(item.value)}
+      ></dw-list-item>
+    `;
+  }
+
+  _onClick(value) {
+    this.value = value;
+    this._opened = false;
+    this.dispatchEvent(new CustomEvent("selection-done", { detail: value }));
   }
 
   get _footerTemplate() {
